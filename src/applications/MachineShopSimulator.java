196c66b5c452dfaabf2a3d7dc8d97f6ffc805301
Dogxx000 <scot0530@morris.umn.edu>
2018-09-23 14:23:30 -0500

    [#2] Change MachineShopSimulator to reflect scope changes to SimulationSpecification

--- src/applications/MachineShopSimulator.java
+++ src/applications/MachineShopSimulator.java
@@ -86,14 +86,14 @@ public class MachineShopSimulator {
         // input the jobs
         Job theJob;
         for (int i = 1; i <= specification.getNumJobs(); i++) {
-            int tasks = specification.getJobSpecifications(i).getNumTasks();
+            int tasks = specification.jobs[i].getNumTasks();
             int firstMachine = 0; // machine for first task
 
             // create the job
             theJob = new Job(i);
             for (int j = 1; j <= tasks; j++) {
-                int theMachine = specification.getJobSpecifications(i).getSpecificationsForTasks()[2*(j-1)+1];
-                int theTaskTime = specification.getJobSpecifications(i).getSpecificationsForTasks()[2*(j-1)+2];
+                int theMachine = specification.jobs[i].getSpecificationsForTasks()[2*(j-1)+1];
+                int theTaskTime = specification.jobs[i].getSpecificationsForTasks()[2*(j-1)+2];
                 if (j == 1)
                     firstMachine = theMachine; // job's first machine
                 theJob.addTask(theMachine, theTaskTime); // add to
