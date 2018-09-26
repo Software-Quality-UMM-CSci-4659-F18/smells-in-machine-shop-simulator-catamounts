e51529369b4871335e9cdc63d22ffcb1967f2b2d
holma198 <holma198@morris.umn.edu>
2018-09-25 21:19:03 -0500

    [#2] Rename various variables and functions Remove useless setters and getters, make associated variables public

--- src/applications/Job.java
+++ src/applications/Job.java
@@ -5,82 +5,46 @@ import dataStructures.LinkedQueue;
 class Job {
     // data members
     private LinkedQueue taskQ; // this job's tasks
-    private int length; // sum of scheduled task times
-    private int arrivalTime; // arrival time at current queue
-    private int id; // job identifier
-    private int numTasks;
+    private int timeRunning; // measures how long the job has been running (excluded downtime)
+    public int machineArrivalTime; // The global time when the job arrives at the machine of the current task.
+    private int id;
+    public int numTasks;
     private int[] specificationsForTasks;
-    private int completionTime;
-    private int totalWaitTime;
+    public int completionTime;
+    public int totalWaitTime;
 
     public int[] getSpecificationsForTasks() {
         return specificationsForTasks;
     }
 
-    // constructor
     Job(int theId) {
         id = theId;
         taskQ = new LinkedQueue();
-        // length and arrivalTime have default value 0
     }
 
-    // other methods
-    public void addTask(int theMachine, int theTime) {
-        getTaskQ().put(new Task(theMachine, theTime));
-    }
-
-    /**
-     * remove next task of job and return its time also update length
-     */
-    public int removeNextTask() {
-        int theTime = ((Task) getTaskQ().remove()).getTime();
-        length = getLength() + theTime;
-        return theTime;
-    }
-
-    public void setNumTasks(int numTasks) {
-        this.numTasks = numTasks;
-    }
-
-    public int getNumTasks() {
-        return numTasks;
-    }
-
-    public void setSpecificationsForTasks(int[] specificationsForTasks) {
-        this.specificationsForTasks = specificationsForTasks;
+    public void addTask(int machineID, int time) {
+        getTaskQ().put(new Task(machineID, time));
     }
 
     public LinkedQueue getTaskQ() {
         return taskQ;
     }
 
-    public int getLength() {
-        return length;
+    public int removeNextTask() {
+        int taskTime = ((Task) getTaskQ().remove()).getTime();
+        timeRunning += taskTime;
+        return taskTime;
     }
 
-    public int getArrivalTime() {
-        return arrivalTime;
+    public void setSpecificationsForTasks(int[] specificationsForTasks) {
+        this.specificationsForTasks = specificationsForTasks;
     }
 
-    public void setArrivalTime(int arrivalTime) {
-        this.arrivalTime = arrivalTime;
+    public int getTimeRunning() {
+        return timeRunning;
     }
 
     public int getId() {
         return id;
     }
-
-    public void setJobCompletionData(int jobNumber, int completionTime, int totalWaitTime) {
-        this.completionTime = completionTime;
-        this.totalWaitTime = totalWaitTime;
-    }
-
-    public int getCompletionTime() {
-        return completionTime;
-    }
-
-    public int getTotalWaitTime() {
-        return totalWaitTime;
-    }
-
 }
