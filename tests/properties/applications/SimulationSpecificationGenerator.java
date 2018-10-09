daf41688de0f41ecc0a1b357339591a45b853d7d
Dogxx000 <scot0530@morris.umn.edu>
2018-10-09 15:39:20 -0500

    [#6] Fix `generate()` in `SimulationSpecificationGenerator` to use updated job[] from `SimulationSpecification`

--- tests/properties/applications/SimulationSpecificationGenerator.java
+++ tests/properties/applications/SimulationSpecificationGenerator.java
@@ -11,9 +11,10 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class SimulationSpecificationGenerator extends Generator<SimulationSpecification> {
-    // I initially thought of having larger numbers here, like 100 or 1,000, but
-    // in playing with properties for `LinkedQueue` it became clear that if your
-    // sizes were too big you never hit edge cases like an empty queue.
+    /* I initially thought of having larger numbers here, like 100 or 1,000, but
+       in playing with properties for `LinkedQueue` it became clear that if your
+       sizes were too big you never hit edge cases like an empty queue.
+    */
     public static final int MAX_MACHINES = 10;
     public static final int MAX_JOBS = 10;
     public static final int MAX_TASKS = 10;
@@ -24,46 +25,38 @@ public class SimulationSpecificationGenerator extends Generator<SimulationSpecif
         super(SimulationSpecification.class);
     }
 
-    @Override public SimulationSpecification generate(SourceOfRandomness r, GenerationStatus status) {
+    @Override
+    public SimulationSpecification generate(SourceOfRandomness r, GenerationStatus status) {
         SimulationSpecification result = new SimulationSpecification();
-        // I add one to these random numbers so I don't end up with 0 machines
-        // or 0 jobs.
+        // Add one so we don't get 0 Machines or Jobs (0 of either breaks test).
         int numMachines = r.nextInt(MAX_MACHINES) + 1;
         int numJobs = r.nextInt(MAX_JOBS) + 1;
 
         result.setNumMachines(numMachines);
         result.setNumJobs(numJobs);
 
-        // Ugh – the annoying problem of the indices starting at one again
-        // so I have to make the array one too large and skip the first
-        // slot.
+        /* changeOverTimes is a 1 indexed array so we need to allocate an extra space */
         int[] changeOverTimes = new int[numMachines + 1];
-        for (int i=1; i<=numMachines; ++i) {
+        for (int i = 1; i <= numMachines; i++) {
             // Changeover times can be 0 so I don't need to add 1 here.
             changeOverTimes[i] = r.nextInt(MAX_CHANGEOVER_TIME);
         }
         result.setChangeOverTimes(changeOverTimes);
 
-        Job[] jobs = new Job[numJobs + 1];
-        for (int i=1; i<=numJobs; ++i) {
+        //CREATING JOBS
+        Job[] jobs = new Job[numJobs];
+        for (int i = 0; i < numJobs; i++) {
+            int numTasks = r.nextInt(MAX_TASKS) + 1; //Because the number of tasks per job cannot be zero
             jobs[i] = new Job(i);
-        }
-        result.jobs = jobs;
-        for (int i=1; i<=numJobs; ++i) {
-            int numTasks = r.nextInt(MAX_TASKS) + 1;
             jobs[i].numTasks = numTasks;
 
-            int[] specificationsForTasks = new int[2 * numTasks + 1];
-
-            for (int j = 1; j <= numTasks; ++j) {
-                int theMachine = r.nextInt(numMachines) + 1;
-                int theTaskTime = r.nextInt(MAX_TASK_TIME) + 1;
-                specificationsForTasks[2 * (j - 1) + 1] = theMachine;
-                specificationsForTasks[2 * (j - 1) + 2] = theTaskTime;
+            for (int j = 0; j < numTasks; j++) {
+                int machine = r.nextInt(numMachines) + 1;
+                int taskTime = r.nextInt(MAX_TASK_TIME) + 1;
+                jobs[i].addTask(machine, taskTime);
             }
-            result.setSpecificationsForTasks(i, specificationsForTasks);
         }
-
+        result.jobs = jobs;
         return result;
     }
 
